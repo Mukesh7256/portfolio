70dfb019d59c57b8450e2d8b31c351df233c5163
Mukesh Kumar <158344795+Mukesh7256@users.noreply.github.com>
2024-11-09 23:13:30 +0530

    something changes in my code

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -3,6 +3,15 @@ import './Hero.css'
 import profile_img from '../../assets/profile_img.jpg'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
 
+  const downloadResume = () => {
+    const link = document.createElement('a');
+    link.href = 'src/assets/Mukesh Keshari CV.pdf';
+
+    link.download = 'src/assets/Mukesh Keshari CV.pdf';
+
+    link.click();
+  };
+
 const Hero = () => {
   return (
     <div className='hero'>
@@ -11,7 +20,7 @@ const Hero = () => {
       <p>I am forntend & backend developer from Intellipaat with 1 year of experience</p>
       <div className="hero-action">
         <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'><p onClick={()=>setMenu("contact")}></p>Connect with me</AnchorLink></div>
-        <div className="hero-resume">My resume</div>
+        <div className="hero-resume" onClick={downloadResume}>My resume</div>
       </div>
     </div>
   )
